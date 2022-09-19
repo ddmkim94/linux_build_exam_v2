fc641f6d87f465c41bb2565c4831d825cf0d4a2b
ddmkim94 <ddmkim94@gmail.com>
2022-09-19 11:22:40 +0900

    test 프로파일 추가

--- src/test/java/com/ll/build/build/BuildApplicationTests.java
+++ src/test/java/com/ll/build/build/BuildApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.build.build;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BuildApplicationTests {
 
 	@Test
