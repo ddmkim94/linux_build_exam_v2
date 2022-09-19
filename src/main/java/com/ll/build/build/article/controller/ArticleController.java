6237c6d036ad899ff2df998d224517f667cec76e
ddmkim94 <ddmkim94@gmail.com>
2022-09-19 11:09:09 +0900

    Article 관련 코드 구현

---
+++ src/main/java/com/ll/build/build/article/controller/ArticleController.java
@@ -0,0 +1,24 @@
+package com.ll.build.build.article.controller;
+
+import com.ll.build.build.article.entity.Article;
+import com.ll.build.build.article.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/article")
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @RequestMapping("/getList")
+    @ResponseBody
+    public List<Article> getList() {
+        return articleService.getList();
+    }
+}
